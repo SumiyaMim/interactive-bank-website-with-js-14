49a2ff71aaa80d5131937e318c048b288b0eca7a
SumiyaMim <sumiyaislam1093@gmail.com>
2023-08-15 11:19:40 +0600

    bank website using js

---
+++ js/deposit.js
@@ -0,0 +1,41 @@
+// step-1: add event listener to the deposit button
+document.getElementById('btn-deposit').addEventListener('click', function(){
+    // console.log('clicked');
+
+    // step-2: set id on the html element then, get the deposit amount from the deposit input field
+    // For input field use .value to the the value inside the input field
+    const depositField = document.getElementById('deposit-field');
+    const depositAmount = depositField.value;
+    const newDepositAmountString = depositField.value;
+    const newDepositAmount = parseFloat(newDepositAmountString);
+
+    // step-7: clear the deposit field
+    depositField.value = '';
+
+    // check NaN
+    if (isNaN(newDepositAmount)) {
+       alert('Please provide a valid number');
+       return;
+    }
+
+    // step-3: get the current deposit total
+    // for non-input(element other than input, textarea) use innerText to get the text
+    const depositTotalElement = document.getElementById('deposit-total');
+    const previousDepositTotalString = depositTotalElement.innerText;
+    const previousDepositTotal = parseFloat(previousDepositTotalString);
+
+    // step-4: add numbers to set the total deposit
+    const currentDepositTotal = previousDepositTotal + newDepositAmount;
+    // set the deposit total
+    depositTotalElement.innerText = currentDepositTotal;
+
+    // step-5: get the current balance total
+    const balanceTotalElement = document.getElementById('balance-total');
+    const previousBalanceTotalString = balanceTotalElement.innerText;
+    const previousBalanceTotal = parseFloat(previousBalanceTotalString);
+
+    // step-6: calculate current total balance / add numbers to set the total balance
+    const currentBalanceTotal = previousBalanceTotal + newDepositAmount;
+    // set the balance total
+    balanceTotalElement.innerText = currentBalanceTotal;
+})
